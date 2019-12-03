5a5be7d5ba1f2f29adf6bdc6f46debcdd3ae81f8
olivierguinel <olivierguinel@gmail.com>
2019-12-03 11:20:35 +0100

    models_references_added

--- app/models/counterpart.rb
+++ app/models/counterpart.rb
@@ -1,2 +1,3 @@
 class Counterpart < ApplicationRecord
+	belongs_to :project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+	belongs_to :organization
+	has_many :counterparts
+	has_many :donations, through: :organizations
+	has_many :donations, through: :counterparts
+	has_many :admins, through: :organizations
 end
