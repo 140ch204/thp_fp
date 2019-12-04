7b032034549ac068d42344ae8f569bb3228604c3
olivierguinel <olivierguinel@gmail.com>
2019-12-04 14:57:19 +0100

    method_user_put_in_userhelper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,19 +26,4 @@ class UsersController < ApplicationController
 	def destroy
 	end
 	
-	private
-
-	def is_master?
-		if !current_user.nil? && current_user.master = true
-			return true
-			flash[:notice] = "You can't access admins' pages."
-		end
-	end
-
-	def check_user
-		if current_user.id != User.find(params[:id]).id
-			flash[:notice] = "You can't see that."
-			redirect_to root_path
-		end
-	end
 end
