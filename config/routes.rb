12fc2a7a67eee64179c16d2f44fb833a9b973362
olivierguinel <olivierguinel@gmail.com>
2019-12-04 13:48:59 +0100

    master_dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  namespace :master do
+      resources :users
+      resources :admins
+      resources :cities
+      resources :counterparts
+      resources :countries
+      resources :departments
+      resources :donations
+      resources :organizations
+      resources :projects
+
+      root to: "users#index"
+    end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
