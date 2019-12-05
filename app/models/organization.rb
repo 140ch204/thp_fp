60bf5e3da60f9d739fc17a22bae2ff42c7b38daf
olivierguinel <olivierguinel@gmail.com>
2019-12-05 10:58:11 +0100

    button_new_asso_project

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,15 @@ class Organization < ApplicationRecord
 	has_many :users, through: :admins
 	has_many :counterparts, through: :donations
 	belongs_to :city
+
+	def is_creating_project_permitted(user)
+		if self.is_association = true && self.is_association_admin(user) && !user.nil?
+			return true
+		end
+	end
+
+	def is_association_admin(user)
+		self.user_ids.include?(user.id) if !user.nil?	
+	end
+
 end
