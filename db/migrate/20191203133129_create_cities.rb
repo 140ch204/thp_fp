322558602abb56d9ff2826aea7d0c48379bba4db
olivierguinel <olivierguinel@gmail.com>
2019-12-06 09:17:25 +0100

    db_in_fixing

--- db/migrate/20191203133129_create_cities.rb
+++ db/migrate/20191203133129_create_cities.rb
@@ -7,5 +7,8 @@ class CreateCities < ActiveRecord::Migration[5.2]
 
 			t.timestamps
 		end
+		add_column :users, :city_id, :belongs_to, index: true
+		add_column :organizations, :city_id, :belongs_to, index: true
+		add_column :projects, :city_id, :belongs_to, index: true
 	end
 end
