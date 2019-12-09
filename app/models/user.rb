79afd83ed402220730f7bb3f1e18eed123eafdd6
140ch204 <140ch204_github@gadz.org>
2019-12-09 10:31:09 +0100

    J06_heroku_mailjet

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   after_create :welcome_send
 
   def welcome_send
-  	UserMailer.welcome_email(self).deliver_now
+  	#UserMailer.welcome_email(self).deliver_now
   end
 
 end
