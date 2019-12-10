d18b30b136c4c45a00a917d36ad54a7019f24bf5
olivierguinel <olivierguinel@gmail.com>
2019-12-10 12:52:30 +0100

    check_admin_on pages_done

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -10,40 +10,47 @@ class Organization < ApplicationRecord
 	has_many :users, through: :admins
 	has_many :counterparts, through: :donations
 	belongs_to :city
-  validates :description, :category,
-    presence: true
-  validates :name,
-    presence: true,
-    uniqueness: true
-  validates :siret, 
-    presence: {message: "doit être fourni"}, if: :company?,
-    uniqueness: true,
-    format: { with: /\A\d{14}\z/}
-  validates :RNA,
-    presence: {message: "doit être fourni"}, if: :association?,
-    uniqueness: true,
-    format: { with: /\A[w]\d{9}\z/i}
+	validates :description, :category,
+	presence: true
+	validates :name,
+	presence: true,
+	uniqueness: true
+	validates :siret, 
+	presence: {message: "doit être fourni"}, if: :company?,
+	uniqueness: true,
+	format: { with: /\A\d{14}\z/}
+	validates :RNA,
+	presence: {message: "doit être fourni"}, if: :association?,
+	uniqueness: true,
+	format: { with: /\A[w]\d{9}\z/i}
 
 
 
+# Il faut être administrateur d'une page pour pouvoir créer un projet.
+# Ainsi, func(is_creating_project_permitted) est notre test administrateur pour une association.
 
-    
-  def association?
-    return true if self.is_association == true
-  end
+	def association?
+		return true if self.is_association == true
+	end
 
-  def company?
-    return true if self.is_company == true
-  end
+	def company?
+		return true if self.is_company == true
+	end
 
 	def is_creating_project_permitted(user)
-		if self.is_association = true && self.is_association_admin(user) && !user.nil?
+		if self.is_association == true && self.is_admin(user) && !user.nil?
 			return true
 		end
 	end
 
-	def is_association_admin(user)
+	def is_admin(user)
 		self.user_ids.include?(user.id) if !user.nil?	
+	end
+
+	def is_organization_admin(user)
+		if self.is_admin(user) && !user.nil?
+			return true
+		end
 	end
 
 end
