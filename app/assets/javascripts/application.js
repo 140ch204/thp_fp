42d2aee4610c355a96d23065f0b19b4b544f2220
mackdatascience007 <mackdatascience@gmail.com>
2019-12-12 20:50:28 +0100

    Add animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,3 @@
 //= require best_in_place
 //= require_tree .
 
-$(document).ready(function() {
-  jQuery(".best_in_place").best_in_place();
-});
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    jQuery(".best_in_place").best_in_place();
+  });
+
