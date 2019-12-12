f54ed672a6c9b0dd17dd756b5b4e36b2b5cafc51
Greutz <Gorton83@gmail.com>
2019-12-12 23:34:49 +0100

    added mails after donation creation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,14 @@ class Donation < ApplicationRecord
 	belongs_to :organization
   belongs_to :project
   validates :donation_amount, numericality: true, presence: true
+
+  after_create :thanks_for_donation_send, :donation_received_send
+
+  def thanks_for_donation_send
+    UserMailer.thanks_for_donation_email(self).deliver_now
+  end
+
+  def donation_received_send
+    UserMailer.donation_received_email(self).deliver_now
+  end
 end
