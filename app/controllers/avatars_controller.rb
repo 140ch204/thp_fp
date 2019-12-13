72316914dde87834873d1784c3b789bf864acae7
olivierguinel <olivierguinel@gmail.com>
2019-12-13 14:53:05 +0100

    profil_done

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,7 +1,12 @@
 class AvatarsController < ApplicationController
-		def create
+	def create
 		@user = User.find(params[:user_id])
-		@user.avatar.attach(params[:avatar])
-		redirect_to user_path(@user)
+		if @user.avatar.attach(params[:avatar])? == true
+			@user.avatar.attach(params[:avatar])
+			redirect_to user_path(@user)
+		else
+			flash[:danger] = "Choisissez une nouvelle photo de profil"
+			redirect_to request.referrer
+		end
 	end
 end
