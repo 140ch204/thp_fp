ab3808c05e50767f8cc976edbeb1845a1b0ad126
mackdatascience007 <mackdatascience@gmail.com>
2019-12-13 13:23:55 +0100

    Background correction

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,8 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user 
-    @url  = 'https://assoangels.herokuapp.com//login' 
+    @url  = 'https://assoangels.herokuapp.com'
+    @url2  = 'https://assoangels.herokuapp.com/users/sign_in'
     mail(to: @user.email, subject: 'Bienvenue chez AssoAngels!') 
   end
 
@@ -11,7 +12,8 @@ class UserMailer < ApplicationMailer
     @project = donation.project
     @company = donation.organization
     @admin_collection = donation.organization.admins
-    @url  = 'https://assoangels.herokuapp.com//login' 
+    @url  = 'https://assoangels.herokuapp.com' 
+    @donation_amount = donation.donation_amount
     @admin_collection.each do |admin|
       mail(to: admin.user.email, subject: 'Merci pour votre don !')
     end
@@ -21,7 +23,8 @@ class UserMailer < ApplicationMailer
     @project = donation.project
     @company = donation.organization
     @admin_collection = donation.project.admins
-    @url = 'https://assoangels.herokuapp.com//login'
+    @url = 'https://assoangels.herokuapp.com'
+    @donation_amount = donation.donation_amount
     @admin_collection.each do |admin|
       mail(to: admin.user.email, subject: "Votre projet #{ @project.name } à reçu un nouveau don !")
     end
