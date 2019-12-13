b086f38967224f4609288b4b55f3902b04dc2ede
olivierguinel <olivierguinel@gmail.com>
2019-12-13 16:14:39 +0100

    test_ok

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,9 +1,9 @@
 class AvatarsController < ApplicationController
 	def create
 		@user = User.find(params[:user_id])
-		if @user.avatar.attach(params[:avatar])? == true
-			@user.avatar.attach(params[:avatar])
-			redirect_to user_path(@user)
+		if @user.avatar.attach(params[:avatar])
+			flash[:success] = "votre photo de profil est enregistrée"
+			redirect_to request.referrer
 		else
 			flash[:danger] = "Choisissez une nouvelle photo de profil"
 			redirect_to request.referrer
